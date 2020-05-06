3386cff8e76e1ecb6eae32576f96d4ecab5db71f
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-05-06 16:32:22 +0430

    Add some flag to unzip

--- get-data.sh
+++ get-data.sh
@@ -4,5 +4,5 @@ export DATA_PATH=$1
 
 DATA_URL="https://www.dropbox.com/sh/64nsb3wrzvmbm85/AABPlZYhunVCx70KYtjDD_D4a?dl=0"
 
-curl -fL $DATA_URL -o $DATA_PATH/data.zip && unzip $DATA_PATH/data.zip -d $DATA_PATH/ && rm -f $DATA_PATH/data.zip
+curl -fL $DATA_URL -o $DATA_PATH/data.zip && unzip -qq $DATA_PATH/data.zip -x / -d $DATA_PATH/ && rm -f $DATA_PATH/data.zip
 
