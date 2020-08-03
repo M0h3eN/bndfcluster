3bfadaa7069f5c7de9a568b8844d9512d0ba03bd
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-08-04 01:24:32 +0430

    Modify hive-init-db service

--- docker-files/docker-hive-metastore-postgresql/init-hive-db.sh
+++ docker-files/docker-hive-metastore-postgresql/init-hive-db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hive WITH PASSWORD 'hive';
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
 
   \c metastore
+
+  \i /hive/hive-schema-2.3.0.postgres.sql
+  \i /hive/hive-txn-schema-2.3.0.postgres.sql
 
   \pset tuples_only
   \o /tmp/grant-privs
