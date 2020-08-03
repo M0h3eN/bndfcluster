991bb1c4a7182f3257dbae234d0b57e8a3f643dc
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-08-04 00:14:44 +0430

    Rollback hive-metastore initiation

--- docker-files/docker-hive-metastore-postgresql/init-hive-db.sh
+++ docker-files/docker-hive-metastore-postgresql/init-hive-db.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hive WITH PASSWORD 'hive';
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
 
   \c metastore
-
-  \i /hive/hive-schema-2.3.0.postgres.sql
-  \i /hive/hive-txn-schema-2.3.0.postgres.sql
-  \i /hive/upgrade-2.3.0-to-3.0.0.postgres.sql
-  \i /hive/upgrade-3.0.0-to-3.1.0.postgres.sql
 
   \pset tuples_only
   \o /tmp/grant-privs
