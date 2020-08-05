ed1a1e5d577e1e6365a682d3ab8a5f5d5736075d
mohsen hadianpour <mohsenhadianpour@gmail.com>
2020-08-06 00:17:16 +0430

    Remove junks in postgres Dockerfile

--- docker-files/docker-hive-metastore-postgresql/init-hive-db.sh
+++ docker-files/docker-hive-metastore-postgresql/init-hive-db.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER hive WITH PASSWORD 'hive';
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
 
   \c metastore
-
-  \i /hive/hive-schema-2.3.0.postgres.sql
-  \i /hive/hive-txn-schema-2.3.0.postgres.sql
 
   \pset tuples_only
   \o /tmp/grant-privs
